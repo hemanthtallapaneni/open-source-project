1bac1d824f5212558680f5272c6e72775798f0d9
hemanthtallapaneni <38141340+hemanthtallapaneni@users.noreply.github.com>
2020-04-11 15:01:37 +0530

    Final upload

---
+++ logindb.php
@@ -0,0 +1,35 @@
+<?php
+$link = mysqli_connect("localhost", "admin", "admin", "employee");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$id = mysqli_real_escape_string($link, $_REQUEST['id']);
+$first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
+$last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO emp (id, first_name, last_name, email) VALUES ('$id', '$first_name', '$last_name', '$email')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+$filter = "SELECT id, first_name, last_name, email FROM emp ORDER BY first_name";
+$result = $conn->query($filter);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. " " . "email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($link);
+?>
